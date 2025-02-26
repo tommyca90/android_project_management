e2e5b455e94fada99ff064fa95406967b84b2732
thomas.calabrese <thomascalabrese1@freenet.de>
2025-02-26 15:10:22 +0100

    add VisibilityIcon and OutlinedPasswordField

---
+++ app/src/main/java/com/example/tcpm/composables/icons/VisibilityIcon.kt
@@ -0,0 +1,20 @@
+package com.example.tcpm.composables.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.ui.res.stringResource
+import com.example.tcpm.R
+
+@Composable
+fun VisibilityIcon(isVisible: Boolean, onChangeVisibility: (Boolean) -> Unit) {
+    val image = if (isVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+    val description = if (isVisible) stringResource(R.string.app_pwd_hide) else stringResource(R.string.app_pwd_show)
+
+    IconButton(onClick = { onChangeVisibility(!isVisible) }) {
+        Icon(imageVector = image, description)
+    }
+}
---
+++ app/src/main/java/com/example/tcpm/composables/inputs/OutlinedPasswordField.kt
@@ -0,0 +1,39 @@
+package com.example.tcpm.composables.inputs
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.example.tcpm.composables.icons.VisibilityIcon
+
+@Composable
+fun OutlinedPasswordField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String)->Unit,
+    label: String
+){
+    var isPwdVisible by rememberSaveable { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        visualTransformation = if (isPwdVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            VisibilityIcon(
+                isPwdVisible,
+                onChangeVisibility = { isVisible -> isPwdVisible = isVisible })
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+    )
+}
