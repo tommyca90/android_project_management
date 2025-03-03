7662bc7db4e4751e2580b9478962652eedf8fb06
thomas.calabrese <thomascalabrese1@freenet.de>
2025-03-03 13:09:21 +0100

    update RoundedTextButton so it is capable of displaying a wait indicator; block RoundedTextButton onClick functionality while waiting is in progress

--- app/src/main/java/com/example/tcpm/composables/buttons/RoundedTextButton.kt
+++ app/src/main/java/com/example/tcpm/composables/buttons/RoundedTextButton.kt
@@ -1,7 +1,10 @@
 package com.example.tcpm.composables.buttons
 
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -19,14 +22,23 @@ fun RoundedTextButton(
     containerColor: Color = colorResource(
         R.color.theme_green
     ),
+    showWaitIndicator: Boolean = false,
     onClick: () -> Unit
 ) {
     TextButton(
         modifier = modifier,
         shape = RoundedCornerShape(6.dp),
         colors = ButtonDefaults.textButtonColors(containerColor = containerColor),
-        onClick = onClick
+        onClick = if(showWaitIndicator) {{}} else onClick
     ) {
-        Text(text, color = textColor)
+        if(showWaitIndicator){
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxHeight(),
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            )
+        }else {
+            Text(text, color = textColor)
+        }
     }
 }
