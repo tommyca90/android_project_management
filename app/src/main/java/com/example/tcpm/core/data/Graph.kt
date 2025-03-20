632158c9d37ff216864fd8a143480128bf09e321
tommyca90 <thomascalabrese@freenet.de>
2025-03-20 15:58:04 +0100

    enable using app with firebase emulators

--- app/src/main/java/com/example/tcpm/core/data/Graph.kt
+++ app/src/main/java/com/example/tcpm/core/data/Graph.kt
@@ -4,8 +4,23 @@ import com.example.tcpm.authentication.data.AuthFirebase
 import com.example.tcpm.authentication.data.AuthProvider
 import com.example.tcpm.user.data.UserFirestore
 import com.example.tcpm.user.data.UserRepository
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 object Graph {
+
+    init {
+        val firestore = FirebaseFirestore.getInstance()
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setHost("10.0.2.2:8080")
+            .setSslEnabled(false)
+            .build()
+        firestore.firestoreSettings = settings
+
+        Firebase.auth.useEmulator("10.0.2.2", 9099)
+    }
 
     val authProvider by lazy {
         AuthProvider(AuthFirebase())
