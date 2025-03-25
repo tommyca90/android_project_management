0fbebf550f06e17617cbe9464b9d16defa5f4fbb
tommyca90 <thomascalabrese@freenet.de>
2025-03-25 18:42:19 +0100

    clear navigation stack when switching between main screens

--- app/src/main/java/com/example/tcpm/navigation/data/NavManager.kt
+++ app/src/main/java/com/example/tcpm/navigation/data/NavManager.kt
@@ -4,22 +4,33 @@ import androidx.navigation.NavController
 import com.example.tcpm.core.presentation.Screen
 
 class NavManager(private val navController: NavController) {
-    fun navigateToLoginRegister(){
-        navController.navigate(Screen.LoginRegisterScreen.route)
+    fun navigateToLoginRegister() {
+        navController.navigate(Screen.LoginRegisterScreen.route) {
+            popUpTo(0) { inclusive = true }
+        }
     }
+
     fun navigateToLogin() {
         navController.navigate(Screen.LoginScreen.route)
     }
-    fun navigateToRegister(){
+
+    fun navigateToRegister() {
         navController.navigate(Screen.RegisterScreen.route)
     }
-    fun navigateToHome(){
-        navController.navigate(Screen.HomeScreen.route)
+
+    fun navigateToHome() {
+        navController.navigate(Screen.HomeScreen.route) {
+            popUpTo(0) { inclusive = true }
+        }
     }
-    fun navigateToAccount(){
-        navController.navigate(Screen.AccountScreen.route)
+
+    fun navigateToAccount() {
+        navController.navigate(Screen.AccountScreen.route){
+            popUpTo(0){ inclusive = true }
+        }
     }
-    fun navigateUp(){
+
+    fun navigateUp() {
         navController.navigateUp()
     }
 }
