02ab7ae8b8167b04e2a29ef42f3c98975cd48dee
tommyca90 <thomascalabrese@freenet.de>
2025-03-26 18:49:07 +0100

    add check if currentUser exists

--- app/src/main/java/com/example/tcpm/authentication/data/AuthFirebase.kt
+++ app/src/main/java/com/example/tcpm/authentication/data/AuthFirebase.kt
@@ -6,17 +6,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
-class AuthFirebase: Authenticator {
+class AuthFirebase : Authenticator {
     private val _auth = FirebaseAuth.getInstance()
 
-    override suspend fun signInWithEmailAndPassword(email: String, password: String): Boolean{
+    override suspend fun signInWithEmailAndPassword(email: String, password: String): Boolean {
         return withContext(Dispatchers.IO) {
             val result = _auth.signInWithEmailAndPassword(email, password).await()
             result.user != null
         }
     }
 
-    override suspend fun createUserWithEmailAndPassword(email: String, password: String): AuthUser{
+    override suspend fun createUserWithEmailAndPassword(email: String, password: String): AuthUser {
         val firebaseUser = withContext(Dispatchers.IO) {
             _auth.createUserWithEmailAndPassword(email, password).await().user
         }
@@ -24,13 +24,21 @@ class AuthFirebase: Authenticator {
     }
 
     override suspend fun currentUser(): AuthUser {
-        return convertToAuthUser(_auth.currentUser)
+        if (_auth.currentUser != null) {
+            return convertToAuthUser(_auth.currentUser)
+        } else {
+            return AuthUser()
+        }
     }
 
     private fun convertToAuthUser(firebaseUser: FirebaseUser?): AuthUser {
         val email = firebaseUser?.email ?: ""
         val userId = firebaseUser?.uid ?: ""
         val isAuthenticated = email != "" && userId != ""
-        return if(isAuthenticated) AuthUser(isAuthenticated = true, email = email, userId = userId) else AuthUser()
+        return if (isAuthenticated) AuthUser(
+            isAuthenticated = true,
+            email = email,
+            userId = userId
+        ) else AuthUser()
     }
 }
